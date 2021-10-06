03cd3e37b2df9d5c4603669ee01e9473a32bf80f
soumyabratakole <soumyabrata.kole@gmail.com>
2021-10-05 17:12:47 -0700

    first-cut

--- simple/decorators.py
+++ simple/decorators.py
@@ -1,3 +1,4 @@
+import time
 from functools import wraps
 
 
@@ -8,11 +9,34 @@ def trace(func, debug=True):
     wraps(func)
     def call(*args, **kwargs):
         if debug:
-            print('Calling', func.__name__,
-                  'With positional arguments', ','.join(map(lambda x: str(x), args)) if args else (),
-                  'and with keyword arguments', ','.join('{}={}'.format(k,v) for k,v in kwargs.items()) if kwargs else {}
-                  )
+            all_args = ', '.join([repr(a) for a in args] +
+                                 [f"{k}={v!r}" for k, v in kwargs.items()])
+            print(f'Calling {func.__name__}({all_args})')
         return func(*args, **kwargs)
     return call
 
 
+def timer(func):
+    """
+    Print the runtime of the decorated function
+    """
+    @wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+
+@trace
+@timer
+def my_add(a,b,c):
+    time.sleep(10)
+    return a + b + c
+
+
+if __name__ == '__main__':
+    my_add(1,2, c=3)
